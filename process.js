f3c1b2e846a8f27b504a8d67be121a375edcd303
Mark Avila <avilamark96@gmail.com>
2021-01-21 20:52:36 +0800

    feat(interface): City and country names change on user search

--- process.js
+++ process.js
@@ -1,17 +1,30 @@
-function fectchWeather() {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=Manila&appid=e1558fe0d8dcc08923d8122663466af2")
-        .then(res => {
-            if(!res.ok){
-                console.log("Response Failed");
-            }
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.log("ERROR");
-        });
+const api = {
+    key: 'e1558fe0d8dcc08923d8122663466af2',
+    link: 'http://api.openweathermap.org/data/2.5/'
 }
 
-fectchWeather();
+const searchInput = document.querySelector('.search');
+
+searchInput.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if(evt.keyCode == 13) {
+        fetchData(searchInput.value);
+    }
+}
+
+function fetchData(query) {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+query+'&units=metric&appid=e1558fe0d8dcc08923d8122663466af2')
+    .then(response => {
+        return response.json();
+    })
+    .then(viewResults);
+}
+
+function viewResults(data) {
+    console.log(data);
+    let city = document.querySelector('.city');
+    let country = document.querySelector('.country');
+    city.innerText = data.name;
+    country.innerText = data.sys.country;
+}
