1ff05836ed95e24126cb0d1b04713255b827cbba
Mark Avila <avilamark96@gmail.com>
2021-06-25 22:20:38 +0800

    fix: bug fixes and code improvements

--- js/process.js
+++ js/process.js
@@ -2,7 +2,7 @@ const searchInput = document.querySelector('.search');
 
 searchInput.addEventListener('keypress', setQuery);
 
-fetchData('Manila'); //display current weather data of manila
+fetchLocation('Manila'); //display current weather data of manila
 
 //set weather map ----------------------
 var weatherMap = L.map('weathermap', {
@@ -19,23 +19,26 @@ weatherMap.setView([14.5, 120.9], 5);
 
 function setQuery(evt) {
     if(evt.keyCode == 13) {
-        fetchData(searchInput.value);
+        fetchLocation(searchInput.value);
     }
 }
 
-function fetchData(query) {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+query+'&units=metric&appid=e1558fe0d8dcc08923d8122663466af2')
-    .then(response => {
-        return response.json();
-    })
-    .then(viewResults);
+async function fetchLocation(query) {
+    
+    let res = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+query+'&units=metric&appid=e1558fe0d8dcc08923d8122663466af2')
+
+    let data = await res.json()
+
+    displayCurrentData(data); 
 }
 
-function fetchForecastData(lat, lon){
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=e1558fe0d8dcc08923d8122663466af2`)
-    .then(response => {
-        return response.json();
-    }).then(viewMoreResults);
+async function fetchForecastData(lat, lon){
+
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=e1558fe0d8dcc08923d8122663466af2`)
+     
+    let data = await res.json()
+
+    displayForecastData(data)
 }
 
 function buildDate(date) {
@@ -60,44 +63,62 @@ function loadingAnim(){
     loading.style.display = "none";
 }
 
-function viewResults(data) {
-    fetchForecastData(data.coord.lat, data.coord.lon); //sends longitude and latitude location to the forecast function
+function displayCurrentData(data) {
 
-    element = {
-        main: {
-            city: document.querySelector('.city'),
-            temp: document.querySelector('.temp'),
-            weather: document.querySelector('.weather'),
-            icon: document.querySelector('.main-icon')
-        },
-        gen: {
-            feels: document.querySelector('#g-feels-like'),
-            locate: document.querySelector("#g-location"),
-            speed: document.querySelector("#wind-speed"),
-            pres: document.querySelector("#pressure"),
-            humid: document.querySelector("#humidity"),
-            visib: document.querySelector("#visibility")
-        }
+    fetchForecastData(data.coord.lat, data.coord.lon); //sends longitude and latitude location to the forecast function
+    
+    $('.city').text(data.name +", "+data.sys.country)
+
+    $('.temp').text(`${Math.round(data.main.temp)}°`)
+    
+    $('.weather').text(toTitleCase(data.weather[0].main))
+    
+    $('.main-icon').html(getWeatherIcon(data.weather[0].id))
+    
+    getWeatherBg(data.weather[0].id)
+    
+    $('#g-feels-like').text(`Feels like ${Math.round(data.main.feels_like)}°C`)
+    
+    $('#g-location').text(data.name +", "+data.sys.country)
+    
+    $('#wind-speed').text(`${data.wind.speed} m/s`)
+    
+    $('#pressure').text(`${data.main.pressure} pHA`)
+    
+    $('#humidity').text(data.main.humidity + ' %')
+    
+    $('#visibility').text(Math.round(data.visibility / 1000) + ' km')
+}
+
+function displayForecastData(data)
+{
+    console.log(data);
+
+    displayChart(data);
+
+    weatherMap.setView([data.lat, data.lon], 8);
+
+    let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    let date = new Date();
+
+    for(var i = 0; i < 8; i++){
+        $(`#weather-${i + 1}`).text(data.daily[i].weather[0].main)
+
+        $(`#day-${i + 1}`).text(week[(date.getDay() + 1 + i) % 7])
+
+        $(`#icon-${i + 1}`).html(getWeatherIcon(data.daily[i].weather[0].id))
+
+        $(`#day-${i + 1}-temp`).text(`${Math.round(data.daily[i].temp.max)} / ${Math.round(data.daily[i].temp.min)}°C`)
     }
 
-    const {city, temp, weather, icon} = element.main;
-    const {feels, speed, pres, humid, visib, locate} = element.gen;
-    
-    //main
-    city.innerText = data.name +", "+data.sys.country;  //Location
-    temp.innerText = `${Math.round(data.main.temp)}°`;  //Location temperaturs
-    weather.innerText = toTitleCase(data.weather[0].main); //location weather
-    icon.innerHTML = getWeatherIcon(data.weather[0].id);//weather icon
-    getWeatherBg(data.weather[0].id)
+    $('#g-current-temp').text(`${Math.round(data.current.temp)}°C`);
 
-    //general (box below)
-    feels.innerText = `Feels like ${Math.round(data.main.feels_like)}°C`;
-    locate.innerText = data.name +", "+data.sys.country; 
-    speed.innerText = `${data.wind.speed} m/s`;
-    pres.innerText = `${data.main.pressure} pHA`;
-    humid.innerText = data.main.humidity + ' %';
-    visib.innerText = Math.round(data.visibility / 1000) + ' km';
+    $('.date').text(moment().tz(`${data.timezone}`).format('dddd, MMMM DD YYYY'));
+
+    $('#g-time').text(moment().tz(`${data.timezone}`).format("YYYY/MM/DD hh:mm"));
 }
+
 
 function toTitleCase(str) {
     
@@ -148,69 +169,21 @@ function getWeatherBg(id){
 
 function getWeatherIcon(id)
 {
+    if(id >= 200 && id <= 232){ return '<i class="fas fa-bolt fa-2x"></i>' } //Thunder
 
-    if(id >= 200 && id <= 232){ //thunder
-        return '<i class="fas fa-bolt fa-2x"></i>';
-    }
-    else if(id >= 300 && id <= 321){ //drizzle
-        return '<i class="fas fa-cloud-rain fa-2x"></i>';
-    }
-    else if(id >= 500 && id <= 531){ //rain
-        return '<i class="fas fa-cloud-showers-heavy fa-2x"></i>';
-    }
-    else if(id >= 600 && id <= 622){ //snow
-        return '<i class="far fa-snowflake fa-2x"></i>';
-    }
-    else if(id >= 700 && id <= 781){ //smog
-        return '<i class="fas fa-smog fa-2x"></i>';
-    }
-    else if(id == 800){             //sunny/clear
-        return '<i class="fas fa-sun fa-2x"></i>';
-    }
-    else if(id >= 801 && id <= 804){ //cloud
-        return '<i class="fas fa-cloud fa-2x"></i>';
-    }
-    else {
-        console.log("Error: weather id not found");
-    }
-
-}
-
-function viewMoreResults(data)
-{
-    console.log(data);
-
-    displayChart(data);
-
-    weatherMap.setView([data.lat, data.lon], 8);
-
-    let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    let city, day, icon, temp, time;
-    let date = new Date();
-    let gtemp = document.querySelector('#g-current-temp');
-
-    for(var i = 0; i < 8; i++){
-        city = document.querySelector(`#weather-${i+1}`);
-        city.innerText = data.daily[i].weather[0].main;
-
-        day = document.querySelector(`#day-${i+1}`);
-        day.innerText = week[(date.getDay() + 1 + i) % 7];
-
-        icon = document.querySelector(`#icon-${i+1}`);
-        icon.innerHTML = getWeatherIcon(data.daily[i].weather[0].id);
-        
-        temp = document.querySelector(`#day-${i+1}-temp`);
-        temp.innerText = `${Math.round(data.daily[i].temp.max)} / ${Math.round(data.daily[i].temp.min)}°C`;
-    }
-
-    gtemp.innerText = `${Math.round(data.current.temp)}°C`;
-
-    let momentdate = document.querySelector('.date');
-    momentdate.innerText = moment().tz(`${data.timezone}`).format('dddd, MMMM DD YYYY');
-
-    time = document.querySelector("#g-time");
-    time.innerText = moment().tz(`${data.timezone}`).format("YYYY/MM/DD hh:mm");
+    else if (id >= 300 && id <= 321){ return '<i class="fas fa-cloud-rain fa-2x"></i>' }  //Drizzle
+    
+    else if(id >= 500 && id <= 531){ return '<i class="fas fa-cloud-showers-heavy fa-2x"></i>' } //rain
+    
+    else if(id >= 600 && id <= 622){ return '<i class="far fa-snowflake fa-2x"></i>' } //Snow
+    
+    else if(id >= 700 && id <= 781){ return '<i class="fas fa-smog fa-2x"></i>' }   //Smog
+    
+    else if(id == 800){ return '<i class="fas fa-sun fa-2x"></i>'} //sunny/clear
+    
+    else if(id >= 801 && id <= 804){ return '<i class="fas fa-cloud fa-2x"></i>' }
+    
+    else { console.log("Error: weather id not found") }
 }
 
 function displayChart(data){
@@ -228,6 +201,7 @@ function displayChart(data){
     ]
 
     let mnt = moment().tz(`${data.timezone}`).format('H') - 1;
+    
     for(var j = 1; j < 11; j++){
        hour.push(time[(mnt + j) % 24]);
     }
